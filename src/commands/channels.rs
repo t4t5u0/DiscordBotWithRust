c91d561fc47a90cef5540bc17acf6d1cb03b1ffa
t4t5u0 <ymmtryk0902@gmail.com>
2021-09-08 01:13:14 +0900

    Update: 全チャンネルを列挙することはできた。

--- src/commands/channels.rs
+++ src/commands/channels.rs
@@ -5,13 +5,43 @@ use serenity::prelude::*;
 #[command]
 #[description = "チャンネル一覧を取得"]
 async fn all_channels(ctx: &Context, msg: &Message) -> CommandResult {
-    let guild_id = *msg.guild_id.unwrap().as_u64();
-    let channels = ctx.http.get_channels(guild_id).await.unwrap();
+    // チャンネル一覧を取得したい
+    // 順番は
+    // text -> voice
+    // カテゴリごとにグルーピングしたい
+    // カテゴリはカテゴリIDごとに取得する
+    // カテゴリIDごとにソート
+    // カテゴリはないこともある
+    // ないやつを優先する
 
-    for chan in channels {
-        msg.channel_id
-            .say(&ctx.http, format!("{}", chan.name))
-            .await?;
+    if let Some(guild_id) = msg.guild_id {
+        let channels = ctx.http.get_channels(*guild_id.as_u64()).await?;
+        for chan in channels {
+            if let Some(category_id) = chan.category_id {
+                // let category = category_id.
+                let channel_name_with_sharp = Channel::Guild(chan.to_owned());
+                msg.channel_id
+                    .say(
+                        &ctx.http,
+                        format!(
+                            "channel_name: {} {}\ncategory_id:   {}\nchannel_kind: {}",
+                            channel_name_with_sharp,
+                            chan.name,
+                            category_id,
+                            chan.kind.name()
+                        ),
+                    )
+                    .await?;
+            } else {
+                // カテゴリ
+                msg.channel_id
+                    .say(&ctx.http, format!("{}", chan.name,))
+                    .await?;
+            }
+        }
+    } else {
+        // なんかバグってる
+        msg.channel_id.say(&ctx.http, "ギルドではない").await?;
     }
     Ok(())
 }
