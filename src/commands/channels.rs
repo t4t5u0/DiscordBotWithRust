33d0c0e3d7d4a26377e714f3a26be002e0c98113
t4t5u0 <ymmtryk0902@gmail.com>
2021-09-09 00:56:43 +0900

    Update: 見にくすぎたので構造体にまとめた

--- src/commands/channels.rs
+++ src/commands/channels.rs
@@ -13,40 +13,50 @@ async fn all_channels(ctx: &Context, msg: &Message) -> CommandResult {
     // カテゴリIDごとにソート
     // カテゴリはないこともある
     // ないやつを優先する
+    // treeコマンドみたいな感じで表示したい
 
     if let Some(guild_id) = msg.guild_id {
-        let channels = ctx.http.get_channels(*guild_id.as_u64()).await?;
-        for chan in channels {
-            let channel_name_with_sharp = Channel::Guild(chan.to_owned());
-            if let Some(category_id) = chan.category_id {
-                msg.channel_id
-                    .say(
-                        &ctx.http,
-                        format!(
-                            "channel_name: {} {}\nchannel_id: {}\ncategory_id:   {}\nchannel_kind: {}",
-                            channel_name_with_sharp,
-                            chan.name,
-                            chan.id,
-                            category_id,
-                            chan.kind.name()
-                        ),
-                    )
-                    .await?;
-            } else {
-                // カテゴリとカテゴリ外チャンネルを見分けることができていない
-                msg.channel_id
-                    .say(
-                        &ctx.http,
-                        format!(
-                            "category_name: {}\ncategory_id: {}\n{}",
-                            chan.name, chan.id, channel_name_with_sharp
-                        ),
-                    )
-                    .await?;
-            }
-        }
+        // 全チャンネルを取得
+        let tmp_channels = ctx.http.get_channels(*guild_id.as_u64()).await?;
+        // イテレーターにしてVecに流し込む
+        let channels = tmp_channels
+            .iter()
+            .map(|chan| MyChannel::new(chan.to_owned()))
+            .collect::<Vec<MyChannel>>();
     } else {
         msg.channel_id.say(&ctx.http, "ギルドではない").await?;
     }
     Ok(())
+}
+
+struct MyChannel {
+    channel_name: String,
+    channel_id: ChannelId,
+    category_id: Option<ChannelId>,
+}
+
+impl MyChannel {
+    fn new(channel: GuildChannel) -> Self {
+        let channel_name_with_sharp = Channel::Guild(channel.to_owned()).to_string();
+        let channel_id = channel.id;
+        let category_id = channel.category_id;
+        let is_category = channel.kind == ChannelType::Category;
+        // カテゴリだったらシャープなし。チャンネルだったらシャープあり
+        let channel_name = if is_category {
+            // これはカテゴリ名
+            channel.name
+        } else {
+            // これはチャンネル名
+            channel_name_with_sharp
+        };
+        Self {
+            channel_name,
+            channel_id,
+            category_id,
+        }
+    }
+}
+
+struct AllChannel {
+    data: Vec<MyChannel>,
 }
